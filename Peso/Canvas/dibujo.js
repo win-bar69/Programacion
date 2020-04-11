6aef117bddc369b717644dfe90d9b641b9f3335b
Winston Barbosa <63030967+win-bar69@users.noreply.github.com>
2020-04-11 16:45:14 -0500

    una linea mas
    3 son mejor

--- Peso/Canvas/dibujo.js
+++ Peso/Canvas/dibujo.js
@@ -2,7 +2,7 @@ var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
 
 lienzo.beginPath();
-lienzo.strokeStyle ="blue";
+lienzo.strokeStyle ="green";
 lienzo.moveTo(10, 100);
 lienzo.lineTo(200, 250);
 lienzo.stroke();
@@ -12,5 +12,12 @@ lienzo.beginPath();
 lienzo.strokeStyle ="red";
 lienzo.moveTo(100, 100);
 lienzo.lineTo(200, 200);
+lienzo.stroke();
+lienzo.closePath();
+
+lienzo.beginPath();
+lienzo.strokeStyle ="yellow";
+lienzo.moveTo(100, 200);
+lienzo.lineTo(100, 50);
 lienzo.stroke();
 lienzo.closePath();
